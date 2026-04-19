ff734e47b0667a3b81f6ec1a0ba3cd346f038263
sooryamannekkattu-arch <sooryamannekkattu@gmail.com>
2026-04-19 21:23:24 +0530

    UC7.java

---
+++ UC7.java
@@ -0,0 +1,21 @@
+public class HelloApp {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+
+            StringBuilder nameBuilder = new StringBuilder();
+
+            
+            for (String name : args) {
+                nameBuilder.append(name).append(", ");
+            }
+
+            
+            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+
+            System.out.println("Hello, " + names + "!");
+        }
+    }
+}
